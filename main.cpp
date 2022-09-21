f6822a6b282c9f748fff1acaf1a21379b4b399fa
danchikov_a <danchikovartem@gmail.com>
2022-09-21 14:46:24 +0300

    Added bmp file and sprite to bmp change

--- main.cpp
+++ main.cpp
@@ -6,6 +6,9 @@
 #include "headers/Sprite.h"
 
 Sprite sprite;
+const int BMP_WIDTH = 56;
+const int BMP_HEIGHT = 170;
+bool isBmp = false;
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam);
 
@@ -52,6 +55,10 @@ int WINAPI wWinMain(HINSTANCE hInstance, HINSTANCE hPrevInstance, PWSTR pCmdLine
 }
 
 LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) {
+    RECT rcClient;
+    GetClientRect(hWnd, &rcClient);
+   // static HBITMAP hBitmap;
+
     switch (uMsg) {
         case WM_MOUSEMOVE: {
             sprite.x = LOWORD(lParam) - sprite.WIDTH / 2;
@@ -75,16 +82,23 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         case WM_KEYDOWN: {
             switch (wParam) {
                 case VK_RIGHT:
-                    sprite.x += sprite.SPEED;
+                    sprite.x + sprite.WIDTH + sprite.SPEED > rcClient.right
+                    ? (sprite.x = rcClient.right - sprite.WIDTH - sprite.SPEED)
+                    : (sprite.x += sprite.SPEED);
                     break;
                 case VK_LEFT:
-                    sprite.x -= sprite.SPEED;
+                    sprite.x - sprite.SPEED < rcClient.left ? (sprite.x = sprite.SPEED) : (sprite.x -= sprite.SPEED);
                     break;
                 case VK_UP:
-                    sprite.y -= sprite.SPEED;
+                    sprite.y - sprite.SPEED < rcClient.top ? (sprite.y = sprite.SPEED) : (sprite.y -= sprite.SPEED);
                     break;
                 case VK_DOWN:
-                    sprite.y += sprite.SPEED;
+                    sprite.y + sprite.HEIGHT + sprite.SPEED > rcClient.bottom
+                    ? (sprite.y = rcClient.bottom - sprite.HEIGHT - sprite.SPEED)
+                    : (sprite.y += sprite.SPEED);
+                    break;
+                case VK_TAB :
+                    isBmp = !isBmp;
                     break;
             }
 
@@ -94,17 +108,30 @@ LRESULT CALLBACK WindowProc(HWND hWnd, UINT uMsg, WPARAM wParam, LPARAM lParam) 
         }
         case WM_DESTROY:
             PostQuitMessage(0);
-
+           // DeleteObject(hBitmap);
             break;
         case WM_PAINT:
             PAINTSTRUCT ps;
             HDC hdc = GetDC(hWnd);
 
             BeginPaint(hWnd, &ps);
+
             FillRect(hdc, &ps.rcPaint, CreateSolidBrush(RGB(255, 255, 255)));
-            HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 67));
-            SelectObject(hdc, hBrush);
-            Ellipse(hdc, sprite.x, sprite.y, sprite.x + sprite.WIDTH, sprite.y + sprite.HEIGHT);
+
+            if(isBmp) {
+                HBITMAP hBmp = (HBITMAP) LoadImage(NULL, L"C:\\Users\\USER\\CLionProjects\\OSISP1\\picture.bmp",
+                                                   IMAGE_BITMAP, 0, 0, LR_LOADFROMFILE);
+                HDC bmpDC = CreateCompatibleDC(hdc);
+                SelectObject(bmpDC, hBmp);
+                BitBlt(hdc, sprite.x + BMP_WIDTH / 2, sprite.y - BMP_HEIGHT / 2,
+                       BMP_WIDTH, BMP_HEIGHT, bmpDC, 0, 0, SRCCOPY);
+                DeleteDC(bmpDC);
+                DeleteObject(hBmp);
+            } else {
+                HBRUSH hBrush = CreateSolidBrush(RGB(255, 0, 67));
+                SelectObject(hdc, hBrush);
+                Ellipse(hdc, sprite.x, sprite.y, sprite.x + sprite.WIDTH, sprite.y + sprite.HEIGHT);
+            }
 
             EndPaint(hWnd, &ps);
             break;
